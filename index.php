fb0c7b799b697481ac0d02cf452edfafae3f3234
Anurag <anumpatel95@gmail.com>
2017-02-10 23:59:15 -0500

    Added php files

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include('includes/header.php'); ?>
+	<div class="starter-template">
+        <h1>My ToDos</h1>
+        <ul class="list-group">
+        	<li class="list-group-item"><a href="todo.php?id=1">SomeThing</a></li>
+        	<li class="list-group-item"><a href="todo.php?id=1">SomeThing</a></li>
+        	<li class="list-group-item"><a href="todo.php?id=1">SomeThing</a></li>
+        	<li class="list-group-item"><a href="todo.php?id=1">SomeThing</a></li>
+      </ul>
+      </div>
+      
+
+<?php include('includes/footer.php'); ?>
